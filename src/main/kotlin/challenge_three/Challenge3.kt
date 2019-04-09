3f8da9e3631471c57d0c895aa9cc26c697f9fcca
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-04-09 09:44:21 +0100

    Refactor out duplicated logic for each move direction

--- src/main/kotlin/challenge_three/Challenge3.kt
+++ src/main/kotlin/challenge_three/Challenge3.kt
@@ -1,45 +1,33 @@
 package challenge_three
 
+enum class Direction(val code: Char, val verticalMovement: Int, val horizontalMovement: Int) {
+    UP('U', -1, 0),
+    DOWN('D', 1, 0),
+    LEFT('L', 0, -1),
+    RIGHT('R', 0, 1);
+
+    companion object {
+        fun of(code: Char): Direction {
+            return Direction.values().single { it.code == code }
+        }
+    }
+}
+
 fun processSokobanMove(board: List<String>, move: Char): List<String> {
-    if (move == 'U') {
-        val rowWithPlayer = board.indexOfFirst { it.contains('p') }
-        val colOfPlayer = board[rowWithPlayer].indexOf('p')
+    val direction = Direction.of(move)
+    val rowWithPlayer = board.indexOfFirst { it.contains('p') }
+    val colOfPlayer = board[rowWithPlayer].indexOf('p')
 
-        return board.map { it.replace('p', ' ') }
-            .mapIndexed { index, s ->
-                if (index == rowWithPlayer - 1) s.replaceRange(colOfPlayer, colOfPlayer + 1, "p")
-                else s
+    return board.map { it.replace('p', ' ') }
+        .mapIndexed { index, row ->
+            if (index == rowWithPlayer + direction.verticalMovement) {
+                row.replaceRange(
+                    colOfPlayer + direction.horizontalMovement,
+                    colOfPlayer + direction.horizontalMovement + 1,
+                    "p"
+                )
+            } else {
+                row
             }
-    }
-    if (move == 'D') {
-        val rowWithPlayer = board.indexOfFirst { it.contains('p') }
-        val colOfPlayer = board[rowWithPlayer].indexOf('p')
-
-        return board.map { it.replace('p', ' ') }
-            .mapIndexed { index, s ->
-                if (index == rowWithPlayer + 1) s.replaceRange(colOfPlayer, colOfPlayer + 1, "p")
-                else s
-            }
-    }
-    if (move == 'L') {
-        val rowWithPlayer = board.indexOfFirst { it.contains('p') }
-        val colOfPlayer = board[rowWithPlayer].indexOf('p')
-
-        return board.map { it.replace('p', ' ') }
-            .mapIndexed { index, s ->
-                if (index == rowWithPlayer) s.replaceRange(colOfPlayer - 1, colOfPlayer, "p")
-                else s
-            }
-    }
-    if (move == 'R') {
-        val rowWithPlayer = board.indexOfFirst { it.contains('p') }
-        val colOfPlayer = board[rowWithPlayer].indexOf('p')
-
-        return board.map { it.replace('p', ' ') }
-            .mapIndexed { index, s ->
-                if (index == rowWithPlayer) s.replaceRange(colOfPlayer + 1, colOfPlayer + 2, "p")
-                else s
-            }
-    }
-    return board
+        }
 }
