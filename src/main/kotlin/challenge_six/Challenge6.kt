3d77e870bb66dc366000aad84b0e12036a5bdecb
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-06-05 23:49:21 +0100

    First attempt at a recursive filter

--- src/main/kotlin/challenge_six/Challenge6.kt
+++ src/main/kotlin/challenge_six/Challenge6.kt
@@ -1,11 +1,14 @@
 package challenge_six
 
-fun myfilter(input: List<Int>, condition: (Int) -> Boolean): List<Int> {
-    val output = mutableListOf<Int>()
+fun myFilter(input: List<Int>, condition: (Int) -> Boolean): List<Int> {
+    if (input.isEmpty()) return emptyList()
 
-    input.forEach {
-        if (condition(it)) output.add(it)
+    if (input.size == 1) {
+        if (condition(input.single())) return listOf(input.single()) else return emptyList()
     }
 
-    return output.toList()
+    val firstItemFiltered = if (condition(input[0])) listOf(input[0]) else emptyList()
+
+    return firstItemFiltered + myFilter(input.drop(1), condition)
+
 }
--- src/test/kotlin/challenge_six/Challenge6KtTest.kt
+++ src/test/kotlin/challenge_six/Challenge6KtTest.kt
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test
 internal class Challenge6KtTest {
     @Test
     fun `should return an empty list when given an empty list`() {
-        val result = myfilter(emptyList()) { true }
+        val result = myFilter(emptyList()) { true }
         assertEquals(emptyList<Int>(), result)
     }
 
     @Test
     fun `should return all items when filter condition is true`() {
         val inputList = listOf(1, 2, 3, 4, 5, 6)
-        val result = myfilter(inputList) { true }
+        val result = myFilter(inputList) { true }
 
         assertEquals(inputList, result)
     }
@@ -21,7 +21,7 @@ internal class Challenge6KtTest {
     @Test
     fun `should return no items when filter condition is false`() {
         val inputList = listOf(1, 2, 3, 4, 5, 6)
-        val result = myfilter(inputList) { false }
+        val result = myFilter(inputList) { false }
 
         assertEquals(emptyList<Int>(), result)
     }
@@ -29,7 +29,7 @@ internal class Challenge6KtTest {
     @Test
     fun `should return even numbers`() {
         val inputList = listOf(1, 2, 3, 4, 5, 6)
-        val result = myfilter(inputList) { it % 2 == 0 }
+        val result = myFilter(inputList) { it % 2 == 0 }
 
         assertEquals(listOf(2, 4, 6), result)
     }
