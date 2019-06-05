e45eeb3a07cc1237e4c390e52185275b86f403a4
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-06-05 23:57:31 +0100

    Remove if statements and replace with a when

--- src/main/kotlin/challenge_six/Challenge6.kt
+++ src/main/kotlin/challenge_six/Challenge6.kt
@@ -1,14 +1,11 @@
 package challenge_six
 
 fun myFilter(input: List<Int>, condition: (Int) -> Boolean): List<Int> {
-    if (input.isEmpty()) return emptyList()
-
-    if (input.size == 1) {
-        return elementIfItMatchesCondition(input.single(), condition)
+    return when (input.size) {
+        0 -> emptyList()
+        1 -> elementIfItMatchesCondition(input.single(), condition)
+        else -> elementIfItMatchesCondition(input.first(), condition) + myFilter(input.drop(1), condition)
     }
-
-    return elementIfItMatchesCondition(input[0], condition) + myFilter(input.drop(1), condition)
-
 }
 
 fun elementIfItMatchesCondition(element: Int, condition: (Int) -> Boolean): List<Int> {
